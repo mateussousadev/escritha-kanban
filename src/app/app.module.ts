d4abd7b9bdc94e6b35eafa3c5ffcd18a9b32dab5
mateussousadev <sousamateus443@gmail.com>
2024-03-22 11:30:08 -0300

    :sparkles: Restarting the project

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MainViewComponent } from './components/main-view/main-view.component';
+
+import { CommonModule } from '@angular/common';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from '@angular/cdk/drag-drop';  
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainViewComponent,
+
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    DragDropModule,
+    CdkDropList,
+  ]
+})
+export class AppModule { }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MainViewComponent } from './components/main-view/main-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: MainViewComponent}
+];
